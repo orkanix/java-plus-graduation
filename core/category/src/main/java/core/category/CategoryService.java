ab1eefd03fd030bd2d8def14c83896e59fb2623a
orkanix <sockolov.44itya@yandex.ru>
2026-01-23 13:08:51 +0300

    Разбиение монолита на модули (добавление оставшихся модулей)

---
+++ core/category/src/main/java/core/category/CategoryService.java
@@ -0,0 +1,12 @@
+package core.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryService.class, args);
+    }
+}
---
+++ core/requests/src/main/java/core/requests/RequestsService.java
@@ -0,0 +1,13 @@
+package core.requests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestsService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestsService.class, args);
+    }
+}
+
