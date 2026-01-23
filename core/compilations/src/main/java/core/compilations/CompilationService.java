68adacd5bd9e967d0b2b2a77f3c62900ca478af6
orkanix <sockolov.44itya@yandex.ru>
2026-01-23 12:53:44 +0300

    Разбиение монолита на модули

---
+++ core/compilations/src/main/java/core/compilations/CompilationService.java
@@ -0,0 +1,12 @@
+package core.compilations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompilationService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompilationService.class, args);
+    }
+}
