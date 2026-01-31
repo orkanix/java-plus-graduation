99f170652cfd17ae641aa10e75b83b6c525ef220
orkanix <sockolov.44itya@yandex.ru>
2026-01-31 22:45:27 +0300

    Исправления названий сервисов

--- core/common/src/main/java/core/common/requests/client/RequestsClient.java
+++ core/common/src/main/java/core/common/requests/client/RequestsClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Set;
 
-@FeignClient(name = "requests-service")
+@FeignClient(name = "request-service")
 public interface RequestsClient {
 
     String PRIVATE_PREFIX = "/users/{userId}/requests";
