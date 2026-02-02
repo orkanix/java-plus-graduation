83f51a3fc67bbefbfe1d34a28cb0fd6b1899c2a2
orkanix <sockolov.44itya@yandex.ru>
2026-02-02 17:43:23 +0300

    Создание сервиса Collector. Описание схем avro и proto. Создание начальной конфигурации для сервисов, взаимодействующих с Kafka

---
+++ telemetry/collector/src/main/java/telemetry/service/collector/Collector.java
@@ -0,0 +1,12 @@
+package telemetry.service.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collector {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Collector.class, args);
+    }
+}
