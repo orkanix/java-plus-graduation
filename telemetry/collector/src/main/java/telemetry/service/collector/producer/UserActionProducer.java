427fef01feab69cffc5fad1c495bbe3e6a326cfb
orkanix <sockolov.44itya@yandex.ru>
2026-02-04 20:19:23 +0300

    Фикс сервиса Collector

--- telemetry/collector/src/main/java/telemetry/service/collector/producer/UserActionProducer.java
+++ telemetry/collector/src/main/java/telemetry/service/collector/producer/UserActionProducer.java
@@ -7,19 +7,20 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class UserActionProducer {
 
-    protected final KafkaTemplate<String, SpecificRecordBase> producer;
+    protected final KafkaTemplate<String, UserActionAvro> producer;
 
     @Value("${spring.topics.user-action-topic-name}")
     private String userActionTopic;
 
-    public void sendAction(SpecificRecordBase userAction) {
-        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+    public void sendAction(UserActionAvro userAction) {
+        ProducerRecord<String, UserActionAvro> record = new ProducerRecord<>(
                 userActionTopic,
                 userAction
         );
